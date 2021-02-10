0b6bd3cc7f7062b8c01a87fba7c2ee3410206b10
Chester Ismay <chester.ismay@gmail.com>
2021-02-09 16:48:23 -0800

    Add prereqs file draft

---
+++ csp-2021/prereqs.R
@@ -0,0 +1,23 @@
+# 1. Ensure you have the current version of R installed
+# by running the following in the Console in RStudio
+R.version.string
+
+## If the results of the above are less than
+## R version 4.0.3, close out of your RStudio and
+## go to https://cloud.r-project.org/
+## and update your R
+
+# 2. Check you have a recent version of RStudio
+# RStudio 1.4.0 or greater recommended
+rstudioapi::versionInfo()$version
+
+## If you need to update, check out
+## https://rstudio.com/products/rstudio/download/#download
+
+# 3. Install the most recent versions of
+# R packages needed for the course
+pkgs <- c(
+  "tidyverse", "moderndive", "dslabs", 
+  "infer", "janitor"
+)
+install.packages(pkgs)
