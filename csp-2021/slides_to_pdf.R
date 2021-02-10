b632edfcc5a245191eaa405db43d2cb8e5ae3604
Chester Ismay <chester.ismay@gmail.com>
2021-02-09 17:13:40 -0800

    Remove extra Rproj step

--- csp-2021/slides_to_pdf.R
+++ csp-2021/slides_to_pdf.R
@@ -1,11 +1,11 @@
 pagedown::chrome_print(
-  "csp-2021/slide_deck.html", 
-  output = "csp-2021/slide_deck.pdf"
+  "slide_deck.html", 
+  output = "slide_deck.pdf"
 )
 
 # To R script
 knitr::purl(
-  "csp-2021/slide_deck.Rmd", 
-  "csp-2021/slide_code.R", 
+  "slide_deck.Rmd", 
+  "slide_code.R", 
   documentation = 2L
 )
